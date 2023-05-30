6c67abb4076d3b4bb241e73eccacd77cea41e465
rachuuday <125911203+rachuuday@users.noreply.github.com>
2023-05-30 23:10:20 +0530

    Add files via upload

---
+++ eclipse-phase4/E-Commerce/src/test/java/com/simplilearn/ECommerceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.simplilearn;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eclipse-phase4/E-Commerce/src/test/java/com/simplilearn/ServletInitializer.java
@@ -0,0 +1,6 @@
+package com.simplilearn;
+
+
+public class ServletInitializer {
+
+}
